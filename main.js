3fc77c9c6518e8ba9c5b9b33ac4354fa46cbe230
erichvvs <erich.adan@gmail.com>
2022-07-18 12:05:45 -0500

    variables-while-if

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-let saludo = "Hola Mundo";
-console.log = (saludo);
+let maximo_calorias = parseFloat(prompt("Ingrese el límite de calorías de tu plan alimenticio:"));
+let entrada = "";
+let consumo = 0;
+
+while (entrada.toUpperCase() != "ESC") {
+    let alimento = prompt("Ingrese el Nombre del Alimento:");
+    console.log("Nombre: " + alimento);
+    let calorias = parseFloat(prompt("Ingrese las Calorías del Alimento Consumido:"));
+    console.log("Calorías: " + calorias);//1950
+    consumo += calorias;
+
+    if (consumo > maximo_calorias) {
+        alert("Superaste el consumo máximo de tu plan alimenticio!");
+        alert("Tus calorías consumidas son de: " + consumo);
+        break;
+    }
+}
